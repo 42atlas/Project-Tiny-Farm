927e0f6283bdd5d524c133f84ddb8e95b8d53c3f
kathy <kathy.s.wu@hotmail.com>
2021-09-28 12:35:26 -0700

    added boilerplate to html and linked files, simple CSS and js to test if linked

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+/*
+  General Assembly
+  Project Zero
+  First Project: Tiny Farm Game
+  Author: Kathy Wu
+  Date: 09/28/21
+*/
+
+console.log(
+  "%c [app.js] loaded!",
+  "background-color:lavender; color:white;",
+);
